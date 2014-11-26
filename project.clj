84529825a8bde09309b23496757f78138a89cb01
Anders Furseth <anders.furseth@gmail.com>
2014-11-26 11:25:16 +0100

    lein-release plugin: preparing 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rubberlike "0.2.0-SNAPSHOT"
+(defproject rubberlike "0.2.0"
   :description "A Clojure library for creating embedded Elasticsearch servers."
   :url "https://github.com/andersfurseth/rubberlike"
   :license {:name "Eclipse Public License"
